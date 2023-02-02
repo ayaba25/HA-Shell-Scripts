2c1734f96c4dab5000b58635fb987dd38abf4688
ayaba25 <111505866+ayaba25@users.noreply.github.com>
2023-02-02 02:20:17 -0700

    Update create-user.sh

--- create-user.sh
+++ create-user.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# improve this scripts and add it to sudoers group.
+# improve this scripts to create user and add to sudoers group.
 echo "Please enter the user name for the account you want to create!"
 read userName
 echo "The name you entered is: "  $userName
